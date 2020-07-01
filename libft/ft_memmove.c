7e3db6e40300aee149368c1d6a5a8ff8da68b01b
Miguel Martin <MiguelMartinn9@gmail.com>
2020-07-01 10:38:39 +0200

    libfit 2 - ft_memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,39 +6,23 @@
 /*   By: mmartin <mmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 09:30:50 by mmartin           #+#    #+#             */
-/*   Updated: 2020/07/01 09:44:14 by mmartin          ###   ########.fr       */
+/*   Updated: 2020/07/01 10:38:06 by mmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
-#include "ft_memcpy.c"
 
-//void	*ft_memmove(void *dst, const void *src, size_t len);
-
-int main()
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char csrc[100] = "Geeksfor";
-	char csrc1[100] = "Geeksfor";
-	char csrc2[100] = "Geeksfor";
+	size_t i;
 
-	printf("\n----\n");
-	memcpy(csrc + 5, csrc, strlen(csrc) + 1);
-	printf("%s", csrc);
-	printf("\n----\n");
-
-	ft_memcpy(csrc1 + 5, csrc1, strlen(csrc1) + 1);
-	printf("%s", csrc1);
-	printf("\n----\n");
-
-	memmove(csrc2 + 5, csrc2, strlen(csrc2) + 1);
-	printf("%s", csrc2);
-	printf("\n----\n");
-	return (0);
+	i = 0;
+	if (dst == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		((char *)dst)[i] = ((char *)src)[i];
+		i++;
+	}
+	return (dst);
 }
-// void	*ft_memmove(void *dst, const void *src, size_t len)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-
