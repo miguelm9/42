aadd14e89f5a5d89e7a64054f8bceaf5eb9fa1c0
Miguel Martin <MiguelMartinn9@gmail.com>
2020-07-01 11:16:04 +0200

    .

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,21 +6,11 @@
 /*   By: mmartin <mmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 10:59:20 by mmartin           #+#    #+#             */
-/*   Updated: 2020/07/01 11:09:22 by mmartin          ###   ########.fr       */
+/*   Updated: 2020/07/01 11:15:46 by mmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
-
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-
-int main(void)
-{
-	printf("%d\n", memcmp("a", "", 4));
-	printf("%d\n", ft_memcmp("a", "", 4));
-	return (0);
-}
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
