9622e6ac720fc04c4a4c8d4141405ec87f32c050
miguelmartinn9 <nerviosus@protonmail.com>
2020-07-05 19:02:21 +0200

    .

--- libft/additional_funcs/ft_strjoin.c
+++ libft/additional_funcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: nerviosus <nerviosus@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/05 18:33:14 by nerviosus         #+#    #+#             */
-/*   Updated: 2020/07/05 18:41:34 by nerviosus        ###   ########.fr       */
+/*   Updated: 2020/07/05 19:02:06 by nerviosus        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,32 @@ size_t ft_strlen(const char *s)
 char *ft_strjoin(char const *s1, char const *s2)
 {
 	char *str;
+	int i;
 
-	str = (char *) malloc((6+ 7 + 1) * sizeof(char));
-
+	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = (char *) malloc(sizeof(*str) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str == NULL)
 		return (NULL);
-
-	while(*s1 != '\0')
+	while (*s1 != '\0')
 	{
-		*str = *s1;
-		printf("Guardando %c de %s\n", *str, str);
+		str[i] = *s1;
 		s1++;
-		str++;
+		i++;
 	}
-	printf("La string: %s\n", str);
 	while(*s2 != '\0')
 	{
-		*str = *s2;
+		str[i] = *s2;
 		s2++;
-		str++;
+		i++;
 	}
-	*str = '\0';
+	str[i] = '\0';
 	return (str);
-
 }
 
 int main(void)
 {
-	printf("%s\n", ft_strjoin("hola ", "mundo!"));
+	printf("%s\n", ft_strjoin("", ""));
 	return (0);	
 }
