d0550f9f87f7b2b4204564a10aaa1369bd68c794
miguelmartinn9 <nerviosus@protonmail.com>
2020-08-10 00:42:46 +0200

    Let's go

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nerviosus <nerviosus@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 18:34:25 by nerviosus         #+#    #+#             */
-/*   Updated: 2020/08/07 15:33:49 by nerviosus        ###   ########.fr       */
+/*   Updated: 2020/08/10 00:42:32 by nerviosus        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,27 @@
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-    unsigned int t;
-    unsigned int p;
-    unsigned int m;
-    unsigned int i;
-   
-    i = -1;
-    t = 0;
-    p = -1;
+	unsigned int t;
+	unsigned int p;
+	unsigned int m;
+	unsigned int i;
 
-    while (dest[++i] != 0)
-    {
-    }
-    m = i;
-    while (src[++p] != 0)
-    {
-    }
-    while (src[t] != 0 && (i < size - 1) && (size != 0))
-    {
-        dest[i] = src[t];
-        i++;
-        t++;
-    }
-    dest[i] = 0;
-    if (size < i)
-        return (size + p);
-    else
-        return (m + p);
+	i = -1;
+	t = 0;
+	p = -1;
+	while (dest[++i] != 0)
+	{
+	}
+	m = i;
+	while (src[++p] != 0)
+	{
+	}
+	while (src[t] != 0 && (i < size - 1) && (size != 0))
+	{
+		dest[i] = src[t];
+		i++;
+		t++;
+	}
+	dest[i] = 0;
+	return (size < i) ? (size + p) : (m + p);
 }
